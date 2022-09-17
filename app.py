40d344cbf20c7de52b7e1c42ceded91876194704
Ritam Majumder <110915978+ritam0210@users.noreply.github.com>
2022-09-18 03:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,3 @@ ip = st.text_input('Enter the tweet') #creates a text box in web app
 op = model.predict([ip])
 if st.button('Predict'):
   st.title(op[0]) # st.button will create a button with name Predict
-  #st.title(op[0]) # the output will be displayed as a title
