a1517f8b504ca38f344a94d657878a55a685321b
Ritam Majumder <110915978+ritam0210@users.noreply.github.com>
2022-09-18 03:07:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import joblib
+
+#import joblib model
+model = joblib.load('tweet_emotions')
+
+st.title('TWEET EMOTIONS CLASSIFIER')#creates a title in web app
+ip = st.text_input('Enter the tweet') #creates a text box in web app
+
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0]) # st.button will create a button with name Predict
+  #st.title(op[0]) # the output will be displayed as a title
